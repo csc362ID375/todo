0c5e3ada322b394aeaf7dd5a4ed1527e6c6d3c0a
Mimi <miughetta@davidson.edu>
2021-03-23 15:43:07 -0400

    updated

--- background.js
+++ background.js
@@ -1,1 +1,38 @@
-console.log("Hello from the background script!");
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log("Hello from the background script!");
+  reset();
+});
+
+function reset() {
+  let obj = {
+    item: [],
+  };
+  localStorage.setItem("iList", JSON.stringify(obj));
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //   console.log(request.action);
+  const obj = JSON.parse(localStorage.getItem("iList")); //getItem(the object name key)
+
+  if(request.action === "delete"){
+    // console.log(request.message);
+
+    // now delete this message from the list
+    for(var i = 0; i < obj.item.length; i++){
+        if(request.message == obj.item[i].message){
+            console.log(obj.item[i].message);
+            obj.item.splice(i,1);
+            localStorage.setItem("iList", JSON.stringify(obj));
+        }
+    }
+    // console.log(obj.item);
+  }
+
+  if (request.action != "getData" && request.action != "delete") {
+    // console.log(request.message);
+    obj.item.push(request);
+    localStorage.setItem("iList", JSON.stringify(obj));
+  }
+
+  sendResponse(obj);
+});
--- content.js
+++ content.js
@@ -1,31 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // get data on load
+  chrome.runtime.sendMessage(
+    {
+      action: "getData",
+    },
+    function (response) {
+      // invoke callback with response
+      // console.log(response["item"].length);
+      for (i = 0; i < response["item"].length; i++) {
+        var li = document.createElement("li");
+        var nextItem = response["item"][i]["message"];
+        // console.log(nextItem);
+        if (nextItem != undefined) {
+          var t = document.createTextNode(nextItem);
+          li.appendChild(t);
+          var span = document.createElement("SPAN");
+          var txt = document.createTextNode("\u00D7");
+          span.className = "close";
+          span.appendChild(txt);
+          li.appendChild(span);
+          document.getElementById("myUL").appendChild(li);
+        }
+      }
+    }
+  );
+
   var btn = document.getElementById("addButton");
 
   //Adding to the list
   btn.addEventListener("click", function () {
-    var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+
     if (inputValue === "") {
       alert("You must write something!");
     } else {
-      document.getElementById("myUL").appendChild(li);    
+      chrome.runtime.sendMessage({ message: inputValue }, function (response) {
+        var li = document.createElement("li");
+        // console.log(response["item"].length);
+        var nextItem = response["item"][response["item"].length - 1]["message"];
+        // console.log(nextItem);
+        if (nextItem != undefined) {
+          var t = document.createTextNode(nextItem);
+          li.appendChild(t);
+          var span = document.createElement("SPAN");
+          var txt = document.createTextNode("\u00D7");
+          span.className = "close";
+          span.appendChild(txt);
+          li.appendChild(span);
+          document.getElementById("myUL").appendChild(li);
+        }
+      });
     }
     document.getElementById("myInput").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-      };
-    }
   });
 
   // Add a "checked" symbol when clicking on a list item
@@ -33,32 +59,44 @@ document.addEventListener("DOMContentLoaded", function () {
   list.addEventListener(
     "click",
     function (ev) {
-      if (ev.target.tagName === "LI") {
+      // var close = document.getElementsByClassName("close");
+
+      if (ev.target.className === "close") {
+        //get rid of this element
+        var elDelete = ev.target.parentElement.innerText;
+        var textelDelete = elDelete.slice(0, elDelete.length - 2);
+
+        chrome.runtime.sendMessage(
+          {
+            action: "delete",
+            message: textelDelete,
+          },
+          function (response) {
+            // invoke callback with response
+            // rewrite the screen
+            document.getElementById("myUL").innerHTML = "";
+            for (i = 0; i < response["item"].length; i++) {
+              var li = document.createElement("li");
+              var nextItem = response["item"][i]["message"];
+              // console.log(nextItem);
+              if (nextItem != undefined) {
+                var t = document.createTextNode(nextItem);
+                li.appendChild(t);
+                var span = document.createElement("SPAN");
+                var txt = document.createTextNode("\u00D7");
+                span.className = "close";
+                span.appendChild(txt);
+                li.appendChild(span);
+                document.getElementById("myUL").appendChild(li);
+              }
+            }
+          }
+        );
+      } else if (ev.target.tagName === "LI") {
+        //mark as checked off
         ev.target.classList.toggle("checked");
       }
     },
     false
   );
-
-  // Click on a close button to hide the current list item
-  var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-
-  //could be used to add anything that is from a stored list?
-  // Create a "close" button and append it to each list item
-  //   var myNodelist = document.getElementsByTagName("LI");
-  //   var i;
-  //   for (i = 0; i < myNodelist.length; i++) {
-  //     var span = document.createElement("SPAN");
-  //     var txt = document.createTextNode("\u00D7");
-  //     span.className = "close";
-  //     span.appendChild(txt);
-  //     myNodelist[i].appendChild(span);
-  //   }
 });
